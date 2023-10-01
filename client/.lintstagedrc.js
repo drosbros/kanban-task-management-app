7024d3c84ed672371b8abb6be8c2c567010a613f
Piotr Dros <piotr.dros01@gmail.com>
2023-10-01 14:55:16 +0200

    chore: update lint staged script

--- client/.lintstagedrc.js
+++ client/.lintstagedrc.js
@@ -1,10 +1,14 @@
 const path = require('path')
 
-const buildEslintCommand = (filenames) =>
+const lint = (filenames) =>
   `next lint --fix --file ${filenames
     .map((f) => path.relative(process.cwd(), f))
     .join(' --file ')}`
 
+const runPrettier = (filenames) =>
+  `yarn prettier --write ${filenames.join(' ')}`
+
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  '*.{js,jsx,ts,tsx}': [lint, runPrettier],
+  '*.(md|json)': [runPrettier],
 }
