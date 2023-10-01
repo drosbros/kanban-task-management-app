adb50f2c84894d02f0a9e2bc2af5a6442186f3d2
Piotr Dros <piotr.dros01@gmail.com>
2023-10-01 15:20:32 +0200

    chore: format code

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,9 +2,9 @@ import Button from '@ui/buttons/Button'
 
 export default function Home() {
   return (
-    <main className="bg-white dark:bg-gray-very-dark min-h-screen grid place-items-center">
-      <div className="">
-        <Button variant={'primary'} size={'large'} className="min-w-[300px]">
+    <main className='bg-white dark:bg-gray-very-dark min-h-screen grid place-items-center'>
+      <div className=''>
+        <Button variant={'primary'} size={'large'} className='min-w-[300px]'>
           Button Primary (L)
         </Button>
         <Button variant={'primary'}>Button Primary</Button>
