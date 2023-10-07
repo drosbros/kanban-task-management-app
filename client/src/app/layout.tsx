4abc8c9aba3833adb07f5bcc4aa7a8fbf3e60245
Piotr Dros <105606398+p-dros@users.noreply.github.com>
2023-10-07 15:20:01 +0200

    DRO-15 introduce state management introduction (#4)
    * chore: add zustand library
    
    * feature: create initial zustand store hook
    
    * chore: store working files before reboot
    
    * chore: improve ssr on images

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Plus_Jakarta_Sans } from 'next/font/google'
 import Navbar from '@/components/Navbar'
-import { ThemeProvider } from '@components/Theme'
+import { ThemeProvider } from '@/components/Theme'
 
 const jakarta = Plus_Jakarta_Sans({ subsets: ['latin'] })
 
