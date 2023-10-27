fb517550955a3e84dc8c88f311ecd0b881b71249
Michał Droś <michal.dros01@gmail.com>
2023-10-27 08:57:03 +0200

    feature: add base grapqhl queries & mutations for issues

--- server/issues/graphql/mutations.py
+++ server/issues/graphql/mutations.py
@@ -6,21 +6,37 @@ from issues.models import Issue
 
 
 class CreateIssue(graphene.Mutation):
+    ok = graphene.Boolean()
+    issue = graphene.Field(IssueType)
+
     class Arguments:
         title = graphene.String()
         description = graphene.String()
         issue_type = graphene.Enum.from_enum(Issue.IssueTypeChoices)()
-        assignee_id = graphene.String(required=False, default_value=None)
-
-    ok = graphene.Boolean()
-    issue = graphene.Field(IssueType)
+        assignee_id = graphene.Int(required=False, default_value=None)
 
     @staticmethod
-    def mutate(_, _info, title: str, description: str, issue_type: Issue.IssueTypeChoices, assignee_id: int | None):
+    def mutate(root, info, title: str, description: str, issue_type: Issue.IssueTypeChoices, assignee_id: int | None):
         assignee = User.objects.filter(pk=assignee_id).first()
         issue = Issue.objects.create(title=title, description=description, type=issue_type, assignee=assignee)
         return CreateIssue(ok=True, issue=issue)
 
 
+class DeleteIssue(graphene.Mutation):
+    ok = graphene.Boolean()
+
+    class Arguments:
+        issue_id = graphene.Int()
+
+    @staticmethod
+    def mutate(_, _info, issue_id: int):
+        issue = Issue.objects.filter(pk=issue_id).first()
+        if issue:
+            issue.delete()
+            return DeleteIssue(ok=True)
+        return DeleteIssue(ok=False)
+
+
 class Mutation(graphene.ObjectType):
     create_issue = CreateIssue.Field()
+    delete_issue = DeleteIssue.Field()
