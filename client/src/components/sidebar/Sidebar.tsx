2a595628872fbac5c0980ea334c8a2c7d0bdcddc
Piotr Droś <piotr.dros01@gmail.com>
2023-11-18 17:11:53 +0100

    refactor: remove unused forward ref

--- client/src/components/sidebar/Sidebar.tsx
+++ client/src/components/sidebar/Sidebar.tsx
@@ -1,22 +1,21 @@
 'use client'
 
-import { ComponentPropsWithRef, forwardRef } from 'react'
-import SidebarHideButton from './SidebarHideButton'
-import CustomThemeSwitcher from './CustomThemeSwitcher'
 import { cn } from '@lib/utils'
 import { useSidebarStore } from '@stores/sidebarStore'
-import SidebarShowButton from './SidebarShowButton'
+import { ComponentPropsWithRef, forwardRef } from 'react'
+import CustomThemeSwitcher from './CustomThemeSwitcher'
 import SidebarBoardSwitcher from './SidebarBoardSwitcher'
+import SidebarHideButton from './SidebarHideButton'
+import SidebarShowButton from './SidebarShowButton'
 
 type Props = ComponentPropsWithRef<'div'>
 
-const Sidebar = forwardRef<HTMLDivElement, Props>(function Sidebar({ className, ...rest }, ref) {
+function Sidebar({ className, ...rest }: Props) {
   const isOpen = useSidebarStore((state) => state.isOpen)
 
   return (
     <>
       <div
-        ref={ref}
         className={cn(
           'left-0 min-w-[18rem] h-full py-8 pb-24 z-20 transition-transform hidden fixed lg:flex flex-col justify-between border-r-2 border-lines-light dark:border-lines-dark bg-white dark:bg-gray-dark cursor-default',
           {
@@ -38,5 +37,5 @@ const Sidebar = forwardRef<HTMLDivElement, Props>(function Sidebar({ className, 
       {!isOpen && <SidebarShowButton className='z-30 absolute bottom-6 left-0' />}
     </>
   )
-})
+}
 export default Sidebar
