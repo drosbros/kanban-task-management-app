8fbc980082655ada17837ba63c319b79239eff75
Piotr Droś <piotr.dros01@gmail.com>
2023-11-21 09:13:51 +0100

    fix: remove unused attribiute

--- client/src/components/board/BoardTask.tsx
+++ client/src/components/board/BoardTask.tsx
@@ -6,10 +6,7 @@ type Props = {
 
 function BoardTask({ task }: Props) {
   return (
-    <div
-      prevent-drag-scroll
-      className='bg-white dark:bg-gray-dark rounded-lg py-4 px-2 prevent-drag-scroll cursor-default min-w-[280px]'
-    >
+    <div className='bg-white dark:bg-gray-dark rounded-lg py-4 px-2 prevent-drag-scroll cursor-default min-w-[280px]'>
       <h3>{task.title}</h3>
     </div>
   )
