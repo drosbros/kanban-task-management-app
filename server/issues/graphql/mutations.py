790dabe4f00fd1a85183f274c37bf37b1463e6ff
Michał Droś <michal.dros01@gmail.com>
2023-12-05 17:49:27 +0100

    feature: add subissue mutations

--- server/issues/graphql/mutations.py
+++ server/issues/graphql/mutations.py
@@ -5,8 +5,8 @@ from accounts.models import User
 from boards.models import Board
 from categories.models import Category
 from commons.utils import graphql_resolver
-from issues.graphql.types import IssueType
-from issues.models import Issue
+from issues.graphql.types import IssueType, SubIssueType
+from issues.models import Issue, SubIssue
 
 
 class CreateIssue(graphene.Mutation):
@@ -69,6 +69,50 @@ class DeleteIssue(graphene.Mutation):
 
         issue.delete()
         return DeleteIssue(ok=True, issue=issue)
+
+
+class CreateSubIssue(graphene.Mutation):
+    ok = graphene.Boolean()
+    subissue = graphene.Field(SubIssueType)
+
+    class Arguments:
+        parent_id = graphene.Int()
+        title = graphene.String()
+        description = graphene.String()
+        assignee_id = graphene.Int(required=False, default_value=None)
+
+    @graphql_resolver
+    def mutate(root, info, parent_id: int, title: str, description: str, assignee_id: int | None):
+        parent_issue = Issue.objects.filter(pk=parent_id).first()
+
+        if not parent_issue:
+            raise GraphQLError("Parent issue not found")
+
+        assignee = User.objects.filter(pk=assignee_id).first()
+
+        subissue = SubIssue.objects.create(
+            parent=parent_issue, title=title, description=description, assignee=assignee, creator=info.context.user
+        )
+
+        return CreateSubIssue(ok=True, subissue=subissue)
+
+
+class DeleteSubIssue(graphene.Mutation):
+    ok = graphene.Boolean()
+    subissue = graphene.Field(IssueType, required=False)
+
+    class Arguments:
+        subissue_id = graphene.Int()
+
+    @graphql_resolver
+    def mutate(_, _info, subissue_id: int):
+        subissue = SubIssue.objects.filter(pk=subissue_id).first()
+
+        if subissue is None:
+            return DeleteSubIssue(ok=False, subissue=None)
+
+        subissue.delete()
+        return DeleteSubIssue(ok=True, subissue=subissue)
 
 
 class Mutation(graphene.ObjectType):
